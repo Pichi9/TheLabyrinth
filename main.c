12d8a48abb472ce0e258e32832c436fe271fe66a
Pichi9 <yacine.mki04@gmail.com>
2023-09-24 20:00:43 +0200

    Fichier main.c avec un Hello World

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL.h>
+
+
+/**
+ * Auteur : MEBARKI Yacine
+ * Fichier : main.c
+*/
+
+
+int main(int argc, char const *argv[])
+{
+	printf("Hello, World!\n");
+	return 0;
+}
